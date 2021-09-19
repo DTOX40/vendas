6fcb139454c242555a3c2a8f4a3504d5216fdf1e
Daniel Candido <daniel.csilva@castgroup.com>
2021-09-19 15:30:07 -0300

    Criando os foms de Fornecedores

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :fornecedores
   resources :produtos
   resources :tipo_produtos
   resources :clientes
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_213103) do
+ActiveRecord::Schema.define(version: 2021_09_19_182755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2021_09_18_213103) do
     t.string "nome"
     t.string "telefone"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fornecedores", force: :cascade do |t|
+    t.string "nome"
+    t.string "telefone"
+    t.string "email"
+    t.string "site"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
