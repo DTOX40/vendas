ebabc07fbd7ecf4012107b6db23cb6a0058456ce
Daniel Candido <daniel.csilva@castgroup.com>
2021-09-20 01:08:21 -0300

    receita de acordo com o mes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
   def index
+    @receitas = Recita.where("created_at BETWEEN ? AND ?", Time.zone.now.beginning_of_month, Time.zone.now.end_of_month).sum(:valor)
+    @despesas = Despesa.where("created_at BETWEEN ? AND ?", Time.zone.now.beginning_of_month, Time.zone.now.end_of_month).sum(:valor)
+    @pedidos = Pedido.where("created_at BETWEEN ? AND ?", Time.zone.now.beginning_of_month, Time.zone.now.end_of_month).sum(:valor_total)
+  end
+
+=begin
+ def index
     @receitas = Recita.sum(:valor)
     @despesas = Despesa.sum(:valor)
     @pedidos = Pedido.sum(:valor_total)
   end
+=end
 end
